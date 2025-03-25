7db4d9522cb4e7b42fd50e6f024c078cb8978e7c
geetaPtongle <geetatongle@gmail.com>
2025-03-25 10:13:11 +0530

    Time: 0 ms (100%), Space: 53.9 MB (32.64%) - LeetHub

---
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+//Approach-1 (Using Queue to simply simulate the operations)
+//T.C : O(n*m) -> Loop runs until queue is empty and in worst case all people have maximum m tickets 
+//S.C : O(n)
+var timeRequiredToBuy1 = function(tickets, k) {
+    let queue = [];
+    let n= tickets.length;
+    let time =0;
+     for (let i = 0; i < n; i++) {
+        queue.push(i);
+        }
+
+     while (queue.length > 0) {
+        time++;
+        let front = queue.shift();
+        
+        tickets[front]--;
+        
+        if (front === k && tickets[front] === 0) {
+            return time;
+        }
+        
+        if (tickets[front] > 0) {
+            queue.push(front);
+        }
+    }
+    return time;
+};
+
+// Approach-3: Using simple observation
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+var timeRequiredToBuy = function(tickets, k) {
+    let time = 0;
+        
+        for (let i = 0; i < tickets.length; i++) {
+            
+            if (i <= k) {
+                time += Math.min(tickets[k], tickets[i]);
+            } else {
+                time += Math.min(tickets[k] - 1, tickets[i]);
+            }
+        }
+        
+        return time;
+};
