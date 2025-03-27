4801eb93a40c7cc243d9c7bbadca83f5cebce190
geetaPtongle <geetatongle@gmail.com>
2025-03-27 10:17:53 +0530

    Time: 2 ms (28.5%), Space: 64.5 MB (6.41%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
@@ -11,20 +11,24 @@
  * @param {TreeNode} root2
  * @return {TreeNode}
  */
-// var mergeTrees = function(root1, root2) {
-//     if (!root1) return root2;  // If root1 is null, return root2
-//     if (!root2) return root1;  // If root2 is null, return root1
 
-//     // Create a new node with sum of both roots' values
-//     let merged = new TreeNode(root1.val + root2.val);
+// \U0001f539 Time Complexity: O(N), where N is the number of nodes in the larger tree.
+// \U0001f539 Space Complexity: O(H), where H is the height of the tree (for recursive stack).
+var mergeTrees = function(root1, root2) {
+    if (!root1) return root2;  // If root1 is null, return root2
+    if (!root2) return root1;  // If root2 is null, return root1
 
-//     // Recursively merge left and right subtrees
-//     merged.left = mergeTrees(root1.left, root2.left);
-//     merged.right = mergeTrees(root1.right, root2.right);
+    // Create a new node with sum of both roots' values
+    let merged = new TreeNode(root1.val + root2.val);
 
-//     return merged;
-// };
+    // Recursively merge left and right subtrees
+    merged.left = mergeTrees(root1.left, root2.left);
+    merged.right = mergeTrees(root1.right, root2.right);
 
+    return merged;
+};
+// \U0001f539 Time Complexity: O(N), where N is the number of nodes in the larger tree.
+// \U0001f539 Space Complexity: O(N), for storing nodes in the queue.
 var mergeTrees = function(root1, root2) {
     if (!root1) return root2;
     if (!root2) return root1;
@@ -49,6 +53,39 @@ var mergeTrees = function(root1, root2) {
             queue.push([node1.right, node2.right]);
         } else if (!node1.right) {
             node1.right = node2.right; // Attach non-null node
+        }
+    }
+
+    return root1;
+};
+
+// \U0001f539 Time Complexity: O(N), same as above.
+// \U0001f539 Space Complexity: O(H), where H is the height of the tree (for the stack).
+
+var mergeTrees = function(root1, root2) {
+    if (!root1) return root2;
+    if (!root2) return root1;
+
+    let stack = [[root1, root2]];
+
+    while (stack.length > 0) {
+        let [node1, node2] = stack.pop();
+
+        // Merge values
+        node1.val += node2.val;
+
+        // Process left children
+        if (node1.left && node2.left) {
+            stack.push([node1.left, node2.left]);
+        } else if (!node1.left) {
+            node1.left = node2.left;
+        }
+
+        // Process right children
+        if (node1.right && node2.right) {
+            stack.push([node1.right, node2.right]);
+        } else if (!node1.right) {
+            node1.right = node2.right;
         }
     }
 
