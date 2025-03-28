d57e10bb0b222d80b166875b3574dd98caea1c0a
geetaPtongle <geetatongle@gmail.com>
2025-03-28 22:38:24 +0530

    Time: 54 ms (48.89%), Space: 58.1 MB (17.75%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -13,22 +13,23 @@
 
 // Time Complexity: O(N)
 // Space Complexity: O(1)
-var detectCycle = function(head) {
-      if(!head || !(head.next)) return null;
-  let slow=fast=head;
-  while(fast && fast.next){
-    slow =slow.next;
-    fast=fast.next.next;
+var detectCycle = function (head) {
+    if (!head || !(head.next)) return null;
+    let slow = fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
 
-    if(slow == fast)
-    break;
-  } 
-  if(fast != slow)
-            return null;
-   slow =head;
-  while(slow != fast){
-    slow=slow.next;
-    fast=fast.next;
-  }
-  return slow;
+        if (slow == fast)
+            break;
+    }
+    if (fast != slow)
+        return null;
+        
+    slow = head;
+    while (slow != fast) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return slow;
 };
