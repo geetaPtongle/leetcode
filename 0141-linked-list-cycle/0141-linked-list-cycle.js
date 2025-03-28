a2879a584f138f87a6b20e4686e4d91848a2c73c
geetaPtongle <geetatongle@gmail.com>
2025-03-28 22:54:51 +0530

    Time: 61 ms (38.46%), Space: 60.1 MB (5.56%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -10,15 +10,23 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function(head) {
-    if(head ==null) return false
-    let slow =fast =head;
-    while(fast && fast.next){
-        slow=slow.next;
-        fast=fast.next.next;
+//  Time Complexity	O(N)
+/* This function uses Floyd’s Tortoise and Hare algorithm (two-pointer     technique).
+*/
+// Space Complexity	O(1)
+/* The algorithm only uses two pointers (slow and fast) and 
+*  a third pointer (p) for tracking the cycle's start.
+*/
+
+var hasCycle = function (head) {
+    if (head == null) return false
+    let slow = fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
 
         if (slow == fast)
             return true;
     }
-     return false;
+    return false;
 };
