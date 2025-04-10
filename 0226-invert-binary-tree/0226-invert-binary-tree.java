2a1d6f0c298d0a36255a333bfeb9571bbb402149
geetaPtongle <geetatongle@gmail.com>
2025-04-10 21:36:15 +0530

    Time: 0 ms (100%), Space: 41.5 MB (25.18%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// TC : O(n), where n is the number of nodes in the binary tree.
+// SC : In the worst case (skewed tree), the depth of recursion is O(n).
+// In the best case (balanced tree), it's O(log n).
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
