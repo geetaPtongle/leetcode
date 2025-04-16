e9572fa2567110a226b00a9998ad42409740b3ff
geetaPtongle <geetatongle@gmail.com>
2025-04-16 13:06:19 +0530

    Time: 0 ms (100%), Space: 41.1 MB (11.93%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximum69Number(int num) {
+        char[] digits = String.valueOf(num).toCharArray();
+
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == '6') {
+                digits[i] = '9';
+                break; // Only change the first 6
+            }
+        }
+        return Integer.parseInt(new String(digits));
+    }
+}
