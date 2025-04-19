303f308a54b35dd43db3d80e703bcf148ad1fdc3
geetaPtongle <geetatongle@gmail.com>
2025-04-19 20:41:01 +0530

    Time: 8 ms (54.37%), Space: 57.1 MB (31.29%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        func(0, s, path, ans);
+        return ans;
+    }
+
+    public void func(int index, String s, List<String> path, List<List<String>> ans) {
+        if(index == s.length()){
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = index; i < s.length(); ++i) {
+            if (isPalindrome(s, index, i)) {
+                path.add(s.substring(index, i+1));
+                func(i+1, s, path, ans);
+                path.remove(path.size() -1);
+            }
+        }
+    }
+
+    public boolean isPalindrome(String s, int start, int end) {
+        while (start <= end) {
+            if (s.charAt(start) != s.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
