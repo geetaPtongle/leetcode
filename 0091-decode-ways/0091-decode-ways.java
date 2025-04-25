f83f20c1a3fd990a3a05d475695b34657d9915b6
geetaPtongle <geetatongle@gmail.com>
2025-04-25 18:12:31 +0530

    Time: 2 ms (17.91%), Space: 41.8 MB (78.19%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,11 +1,31 @@
 class Solution {
     Integer[] memo;
     int [] dp;
-    public int numDecodings(String s) {
-        // memo = new Integer[s.length() + 1];
-        // Arrays.fill(memo, -1);
-        // return solve(0, s);
+    public int numDecodings1(String s) {
+        memo = new Integer[s.length() + 1];
+        Arrays.fill(memo, -1);
+        return solve(0, s);    
+    }
 
+    public int solve(int index, String s) {
+        if (index == s.length())
+            return 1;
+        if (s.charAt(index) == '0') {
+            return 0;
+        }
+        if (memo[index] != -1)
+            return memo[index];
+        int pick1 = solve(index + 1, s);
+        int pick2 = 0;
+        if (index < s.length() - 1 && Integer.parseInt(s.substring(index, index + 2)) <= 26) {
+            pick2 = solve(index + 2, s);
+        }
+        memo[index] = pick1 + pick2;
+        return memo[index];
+    }
+
+    public int numDecodings(String s) {
+    
         if (s == null || s.length() == 0 || s.charAt(0) == '0') return 0;
 
         int n = s.length();
@@ -28,26 +48,7 @@ class Solution {
             }
         }
 
-        return dp[n];
+        return dp[n];   
     }
-
-    public int solve(int index, String s) {
-        if (index == s.length())
-            return 1;
-        if (s.charAt(index) == '0') {
-            return 0;
-        }
-        if (memo[index] != -1)
-            return memo[index];
-        int pick1 = solve(index + 1, s);
-        int pick2 = 0;
-        if (index < s.length() - 1 && Integer.parseInt(s.substring(index, index + 2)) <= 26) {
-            pick2 = solve(index + 2, s);
-        }
-        memo[index] = pick1 + pick2;
-        return memo[index];
-    }
-
-
      
 }
