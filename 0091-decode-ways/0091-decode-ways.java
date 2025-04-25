cac3c725fc9ae490362da46ac3053e636b4c8512
geetaPtongle <geetatongle@gmail.com>
2025-04-25 19:23:07 +0530

    Time: 1 ms (84.05%), Space: 41.9 MB (50.79%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,52 +1,60 @@
 class Solution {
     Integer[] memo;
-    int [] dp;
+    int[] dp;
+
     public int numDecodings1(String s) {
         memo = new Integer[s.length() + 1];
         Arrays.fill(memo, -1);
-        return solve(0, s);    
+        return solve(0, s);
     }
 
     public int solve(int index, String s) {
         if (index == s.length())
             return 1;
+
         if (s.charAt(index) == '0') {
             return 0;
         }
         if (memo[index] != -1)
             return memo[index];
+
         int pick1 = solve(index + 1, s);
+
         int pick2 = 0;
+
         if (index < s.length() - 1 && Integer.parseInt(s.substring(index, index + 2)) <= 26) {
             pick2 = solve(index + 2, s);
         }
+
         memo[index] = pick1 + pick2;
+
         return memo[index];
     }
 
     public int numDecodings(String s) {
-        if (s.charAt(0) == '0') return 0;
+        if (s.charAt(0) == '0')
+            return 0;
         int n = s.length();
         dp = new int[n + 1];
 
-        dp[0] = 1;  // Base case for empty string
-        dp[1]= 1;  // Base case for the first character
+        dp[0] = 1; // Base case for empty string
+        dp[1] = 1; // Base case for the first character
 
         for (int i = 2; i <= n; i++) {
-             char chi = s.charAt(i-1);
-            char chim1 = s.charAt(i-2);
-            String st = s.substring(i-2, i);
+            char chi = s.charAt(i - 1);
+            char chim1 = s.charAt(i - 2);
+            String st = s.substring(i - 2, i);
 
-            if(chi != '0') {
-                dp[i] = dp[i-1];
+            if (chi != '0') {
+                dp[i] = dp[i - 1];
             }
 
-            if(chim1 != '0' && Integer.parseInt(st) <= 26) {
-                dp[i] += dp[i-2];
+            if (chim1 != '0' && Integer.parseInt(st) <= 26) {
+                dp[i] += dp[i - 2];
             }
         }
 
-        return dp[n];   
+        return dp[n];
     }
-     
+
 }
