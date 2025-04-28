2194a4e3fc35174729c1b54d340dad0b9db79821
geetaPtongle <geetatongle@gmail.com>
2025-04-28 20:01:28 +0530

    Time: 1252 ms (6.39%), Space: 45.3 MB (6.02%) - LeetHub

--- 2031-egg-drop-with-2-eggs-and-n-floors/2031-egg-drop-with-2-eggs-and-n-floors.java
+++ 2031-egg-drop-with-2-eggs-and-n-floors/2031-egg-drop-with-2-eggs-and-n-floors.java
@@ -4,21 +4,21 @@ class Solution {
             return findFloor(2, n);
     }
 
-    public int findFloor(int k, int n) {
+    public int findFloor(int eggs, int n) {
         if (n <= 1)
             return n;
-        if (k == 1)
+        if (eggs == 1)
             return n;
 
-        String temp = findString(k, n);
+        String temp = findString(eggs, n);
         if (mp.containsKey(temp))
             return mp.get(temp);
 
         int minSteps = Integer.MAX_VALUE;
         for (int floor = 1; floor < n; floor++) {
             // 2 options
-            int op1 = findFloor(k - 1, floor - 1); // eggs break from floor and reducing floor by 1  floor 
-            int op2 = findFloor(k, n - floor); // eggs not break icrease the floor from current floor which is n-i
+            int op1 = findFloor(eggs - 1, floor - 1); // eggs break from floor and reducing floor by 1  floor 
+            int op2 = findFloor(eggs, n - floor); // eggs not break icrease the floor from current floor which is n-i
             int steps = 1 + Math.max(op1, op2);
             minSteps = Math.min(minSteps, steps);
         }
