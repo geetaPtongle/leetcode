ef412468148b1575a4c6de153f93bf08e7e0bf88
geetaPtongle <geetatongle@gmail.com>
2025-04-29 22:31:23 +0530

    Time: 29 ms (32.06%), Space: 41.9 MB (31.18%) - LeetHub

--- 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.java
+++ 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.java
@@ -1,7 +1,12 @@
 class Solution {
-     private int[][] t;
+    private int[][] t;
+
     public int minDifficulty(int[] jobDifficulty, int d) {
-        int n=jobDifficulty.length;
+        int n = jobDifficulty.length;
+        
+        if (n < d)
+            return -1;
+
         t = new int[n][d + 1];
         for (int[] row : t) {
             Arrays.fill(row, -1);
@@ -9,19 +14,29 @@ class Solution {
         return solve(jobDifficulty, n, 0, d);
     }
 
-    public int solve(int[] jobDifficulty, int n, int index, int d){
-         if (n - index < d) return -1;
-         if (d == 1) {
-            return Arrays.stream(Arrays.copyOfRange(jobDifficulty, index, n)).max().orElse(Integer.MIN_VALUE);
+    public int solve(int[] jobDifficulty, int n, int index, int d) {
+
+        if (d == 1) {
+            int maxD= jobDifficulty[index];
+            for(int i=index; i<n; i++){
+                 maxD =Math.max(maxD, jobDifficulty[i]);
+                
+            }
+            return maxD;
+            // return Arrays.stream(Arrays.copyOfRange(jobDifficulty, index, n)).max().orElse(Integer.MIN_VALUE);
+        
         }
         if (t[index][d] != -1)
             return t[index][d];
+
         int maxDifficulty = Integer.MIN_VALUE;
         int result = Integer.MAX_VALUE;
+
         for (int i = index; i <= n - d; i++) {
             maxDifficulty = Math.max(maxDifficulty, jobDifficulty[i]);
             result = Math.min(result, maxDifficulty + solve(jobDifficulty, n, i + 1, d - 1));
         }
+
         return t[index][d] = result;
     }
 }
