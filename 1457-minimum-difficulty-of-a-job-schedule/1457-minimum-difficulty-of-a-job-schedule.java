aa1e8d4306a96e009055d63e912418b1fd7a4c1d
geetaPtongle <geetatongle@gmail.com>
2025-04-29 22:23:54 +0530

    Time: 176 ms (5.23%), Space: 44.9 MB (8.01%) - LeetHub

---
+++ 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.java
@@ -0,0 +1,27 @@
+class Solution {
+     private int[][] t;
+    public int minDifficulty(int[] jobDifficulty, int d) {
+        int n=jobDifficulty.length;
+        t = new int[n][d + 1];
+        for (int[] row : t) {
+            Arrays.fill(row, -1);
+        }
+        return solve(jobDifficulty, n, 0, d);
+    }
+
+    public int solve(int[] jobDifficulty, int n, int index, int d){
+         if (n - index < d) return -1;
+         if (d == 1) {
+            return Arrays.stream(Arrays.copyOfRange(jobDifficulty, index, n)).max().orElse(Integer.MIN_VALUE);
+        }
+        if (t[index][d] != -1)
+            return t[index][d];
+        int maxDifficulty = Integer.MIN_VALUE;
+        int result = Integer.MAX_VALUE;
+        for (int i = index; i <= n - d; i++) {
+            maxDifficulty = Math.max(maxDifficulty, jobDifficulty[i]);
+            result = Math.min(result, maxDifficulty + solve(jobDifficulty, n, i + 1, d - 1));
+        }
+        return t[index][d] = result;
+    }
+}
