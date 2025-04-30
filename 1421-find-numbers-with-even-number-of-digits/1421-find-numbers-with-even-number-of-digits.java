ba20b7863dba1a0be8070f46a1118bd01061758d
geetaPtongle <geetatongle@gmail.com>
2025-04-30 13:56:37 +0530

    Time: 2 ms (26.47%), Space: 44.1 MB (12.57%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
@@ -1,14 +1,44 @@
 class Solution {
-    public int findNumbers(int[] nums) {
-        int n=nums.length;
-        int result =0;
-        for(int i=0; i<n; i++){
-            if((nums[i] >=10 && nums[i]<=99) ||
-            (nums[i] >=1000 && nums[i]<=9999)||
-            ( nums[i]==100000)){
+    // approch1
+    public int findNumbers1(int[] nums) {
+        int n = nums.length;
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            if ((nums[i] >= 10 && nums[i] <= 99) ||
+                    (nums[i] >= 1000 && nums[i] <= 9999) ||
+                    (nums[i] == 100000)) {
                 result++;
             }
         }
         return result;
+    }
+
+    // approch2
+    public int findNumbers2(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            int digits = 0;
+            int n = num;
+            while (n != 0) {
+                digits++;
+                n /= 10;
+            }
+            if (digits % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        String[] strArray = new String[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            strArray[i] = String.valueOf(nums[i]);
+             if (strArray[i].length() % 2 == 0) 
+                count++;
+        }
+        return count;
     }
 }
