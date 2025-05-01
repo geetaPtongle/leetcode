5bd5aa3e11247bcc4f13bcc0eabd40f443cdad7b
geetaPtongle <geetatongle@gmail.com>
2025-05-01 16:52:25 +0530

    Time: 33 ms (64.62%), Space: 42.9 MB (61.37%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,22 +1,37 @@
 class Solution {
-    int []dp;
-    public int numSquares(int n) {
-        dp=new int[n + 1];
+    int[] dp;
+
+    public int numSquares1(int n) {
+        dp = new int[n + 1];
         Arrays.fill(dp, -1);
-      return solve(n);
+        return solve(n);
     }
 
-    public int solve(int n){
-        if (n==0) return 0;
-        if(dp[n] != -1){
+    public int solve(int n) {
+        if (n == 0)
+            return 0;
+        if (dp[n] != -1) {
             return dp[n];
         }
-        int minValue= Integer.MAX_VALUE;
-        for(int i=1; i*i <=n; i++){
-            int square = i*i;
-            minValue=Math.min(minValue, 1+ solve(n-square));
+        int minValue = Integer.MAX_VALUE;
+        for (int i = 1; i * i <= n; i++) {
+            int square = i * i;
+            minValue = Math.min(minValue, 1 + solve(n - square));
         }
-         dp[n]=minValue;
+        dp[n] = minValue;
         return minValue;
     }
+
+       public int numSquares(int n) {
+        int[] memo =new int[n+1];
+        Arrays.fill(memo, Integer.MAX_VALUE);
+        memo[0] =0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j*j <=i; j++){
+                int square=j*j;
+                memo[i] = Math.min(memo[i], memo[i -square]+1);
+            }
+        }
+        return memo[n];
+       }
 }
