b14f68d2f8566b6e672c19afdef7c75bde2d66f3
geetaPtongle <geetatongle@gmail.com>
2025-05-03 14:29:25 +0530

    Time: 6 ms (13%), Space: 47.7 MB (45.6%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,36 +1,86 @@
 class Solution {
-    public int minPathSum(int[][] grid) {
-         int m =grid.length;
-        int n =grid[0].length;
+    public int minPathSum1(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
 
         // int result =Integer.MAX_VALUE;
-         int memo[][] = new int[m][n];
+        int memo[][] = new int[m][n];
         for (int[] arr : memo) {
             Arrays.fill(arr, Integer.MAX_VALUE);
         }
         //travlng form 0 to n cell
-      return solve(m - 1, n - 1, memo, grid);
-         
+        return solve(m - 1, n - 1, memo, grid);
+
     }
 
-     public int solve(int i, int j,int[][] memo, int [][] grid) {
-    if (i < 0 || j < 0) return Integer.MAX_VALUE;
+    public int solve(int i, int j, int[][] memo, int[][] grid) {
+        if (i < 0 || j < 0)
+            return Integer.MAX_VALUE;
 
-    if (i == 0 && j == 0) {
-        return grid[i][j];
-    }
+        if (i == 0 && j == 0) {
+            return grid[i][j];
+        }
 
-    if (memo[i][j] != Integer.MAX_VALUE) {
-        return memo[i][j];
-    }
+        if (memo[i][j] != Integer.MAX_VALUE) {
+            return memo[i][j];
+        }
 
-    int up =solve(i - 1, j, memo, grid);
-    int left = solve(i, j - 1, memo, grid);
+        int up = solve(i - 1, j, memo, grid);
+        int left = solve(i, j - 1, memo, grid);
 
-
-  int minUp = up != Integer.MAX_VALUE ? up + grid[i][j] : Integer.MAX_VALUE;
+        int minUp = up != Integer.MAX_VALUE ? up + grid[i][j] : Integer.MAX_VALUE;
         int minLeft = left != Integer.MAX_VALUE ? left + grid[i][j] : Integer.MAX_VALUE;
 
-    return memo[i][j] = Math.min(minUp , minLeft);
+        return memo[i][j] = Math.min(minUp, minLeft);
     }
+
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else {
+                    int up = Integer.MAX_VALUE;
+                    int left = Integer.MAX_VALUE;
+                    if (i > 0)
+                        up = grid[i][j] + dp[i - 1][j];
+                    if (j > 0)
+                        left = grid[i][j] + dp[i][j - 1];
+                    dp[i][j] = Math.min(up, left);
+                }
+
+            }
+        }
+        return dp[m - 1][n - 1];
+
+    }
+
+// public int minPathSum(int[][] grid) {
+//     int m = grid.length;
+//     int n = grid[0].length;
+//     int[][] dp = new int[m][n];
+
+//     for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//             if (i == 0 && j == 0) {
+//                 dp[i][j] = grid[i][j];
+//             } else {
+//                 int up = Integer.MAX_VALUE;
+//                 int left = Integer.MAX_VALUE;
+//                 if (i > 0)
+//                     up = grid[i][j] + dp[i - 1][j];
+//                 if (j > 0)
+//                     left = grid[i][j] + dp[i][j - 1];
+//                 dp[i][j] = Math.min(up, left);
+//             }
+//         }
+//     }
+
+//     return dp[m - 1][n - 1];
+// }
+
 }
