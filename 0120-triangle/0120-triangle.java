9372d9b757b94b9a6dc8daec9ffee0a667156b70
geetaPtongle <geetatongle@gmail.com>
2025-05-03 14:53:57 +0530

    Time: 1 ms (99.75%), Space: 44.4 MB (44.92%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -8,9 +8,12 @@ class Solution {
     public int solve(int i, int j, List<List<Integer>> triangle, Integer[][] dp){
          int m = triangle.size();
          if (dp[i][j] != null) return dp[i][j];
+
         if( i== m-1) return triangle.get(i).get(j);
+
         int down =  triangle.get(i).get(j) + solve(i+1, j, triangle, dp);
         int diago =  triangle.get(i).get(j) + solve(i+1, j+1, triangle,dp);
+        
         return dp[i][j] =Math.min(down,diago);
     }
 }
