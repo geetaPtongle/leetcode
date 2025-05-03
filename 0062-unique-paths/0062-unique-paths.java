46e39f9a444b39d46e6658363fc9eb83d9762f1c
geetaPtongle <geetatongle@gmail.com>
2025-05-03 12:53:28 +0530

    Time: 0 ms (100%), Space: 40.6 MB (48.44%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,84 @@
+class Solution {
+
+    //TC: O(m*n)
+    //SC: dp O(m*n) + recurtion O(m-1) + O(n-1) which is nothing but path size m-1 -> and n-1 ->0
+    public int uniquePaths1(int m, int n) {
+        int memo[][] = new int[m][n];
+        for (int[] arr : memo) {
+            Arrays.fill(arr, -1);
+        }
+        //travlng form 0 to n cell
+        // return solve(0, 0, m, n, memo);
+
+        //travlng form 0 to n cell
+        return solve(0, 0, m, n, memo);
+    }
+
+    //travlng form 0 to n cell
+    public int solve1(int i, int j, int m, int n, int[][] memo) {
+        if (i == m - 1 && j == n - 1) {
+            return 1;
+        }
+
+        // Out of bounds
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return 0;
+        }
+
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+        
+
+        int right = solve(i, j + 1, m, n, memo);
+        int down = solve(i + 1, j, m, n, memo);
+
+        return memo[i][j] = right + down;
+    }
+
+    //travlng form n to 0 cell
+    public int solve(int i, int j, int m, int n, int[][] memo) {
+        if (i == 0 && j ==0) {
+            return 1;
+        }
+
+        // Out of bounds
+        if (i < 0 || j < 0) {
+            return 0;
+        }
+
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+        
+
+        int right = solve(i-1, j, m, n, memo);
+        int down = solve(i, j+1, m, n, memo);
+
+        return memo[i][j] = right + down;
+    }
+
+ public int uniquePaths(int m, int n ){
+   int[][] dp = new int[m][n];
+   dp[0][0] =1;
+
+  for(int i=0; i<m; i++){
+    for(int j=0; j<n; j++){
+        
+        if(i ==0 && j ==0){
+            dp[i][j]= 1;
+            continue;
+        }
+        int up=0;
+        int left=0;
+       
+            if (i - 1 >= 0) up = dp[i - 1][j];
+            if (j - 1 >= 0) left = dp[i][j - 1];
+
+        dp[i][j] = up +left;
+    }
+  }
+  return dp[m-1][n-1];
+ }
+
+}
