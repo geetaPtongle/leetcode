1b747831a6ae8308f22c181e24831d652df2a0d4
geetaPtongle <geetatongle@gmail.com>
2025-05-06 17:31:01 +0530

    Time: 37 ms (78.1%), Space: 53.2 MB (67.56%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.js
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.js
@@ -5,7 +5,14 @@
 var defangIPaddr = function (address) {
     let n = address.length;
     let result = '';
-    for (let i = 0; i < n; i++)
-        result += address[i] === '.' ? '[.]' : address[i];
+    // for (let i = 0; i < n; i++)
+    //     result += address[i] === '.' ? '[.]' : address[i];
+
+    // result = address.replaceAll('.', '[.]');
+
+    // result = address.split('.').join("[.]");
+
+    result = Array.from(address).map((element) => element ==='.' ? '[.]': element).join('');
+
     return result;
 };
