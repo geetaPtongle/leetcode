0c3b6e2ba58374e3671ef3c4b3205ae44f391f41
geetaPtongle <geetatongle@gmail.com>
2025-05-06 15:02:40 +0530

    Time: 2 ms (7.3%), Space: 45.4 MB (44.27%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -10,26 +10,12 @@ class Solution {
 
     public int[] buildArray(int[] nums) {
         int n = nums.length;
-        // int msb = (int) ((Math.log(n) / Math.log(2)) + 1);
-        // int mask = (1 << msb) - 1;
-
-        // // binary String :: new Value....old Value
-        // for (int i = 0; i < n; i++) {
-        //     nums[i] |= (nums[nums[i]] & mask) << msb;
-        // }
-
-        // for (int i = 0; i < n; i++) {
-        //     nums[i] >>= msb;        // remove old value
-        // }
-
         for(int i=0; i<n; i++){
             nums[i] =nums[i] + n*(nums[nums[i]] %n);
         }
-
         for(int i=0;i<n;i++){
             nums[i]=nums[i]/n;
         }
-
         return nums;
     }
 }
