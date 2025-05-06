e6c80af32f419e160bba492cb0d3a1bf3ca656f7
geetaPtongle <geetatongle@gmail.com>
2025-05-06 23:32:01 +0530

    Time: 1 ms (70.4%), Space: 41.4 MB (81.54%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<String> st = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch != ']') {
+                st.push(String.valueOf(ch));
+            } else {
+                StringBuilder buildString = new StringBuilder();
+
+                while (!st.isEmpty() && !st.peek().equals("[")) {
+                    buildString.insert(0, st.pop());
+                }
+                st.pop();
+
+                StringBuilder digit = new StringBuilder();
+
+                while (!st.isEmpty()) {
+                    String top = st.peek();
+                    if (!Character.isDigit(top.charAt(0)))
+                        break;
+                    digit.insert(0, st.pop());
+                }
+
+                int repeat = Integer.parseInt(digit.toString());
+                StringBuilder expanded = new StringBuilder();
+                for (int j = 0; j < repeat; j++) {
+                    expanded.append(buildString);
+                }
+                st.push(expanded.toString());
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (String str : st) {
+            result.append(str);
+        }
+        return result.toString();
+    }
+}
