b7902fe5cf4fe83f1f86b84261be240fc8bf8b32
geetaPtongle <geetatongle@gmail.com>
2025-05-06 21:11:31 +0530

    Time: 17 ms (6.29%), Space: 58.7 MB (77.11%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,16 +9,42 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        List<Integer> list1 =new ArrayList<>();
-        
-        ListNode current =head;
-        while(current !=null){
+    //Approch one using Collections.resverse method 
+    public boolean isPalindrome1(ListNode head) {
+        List<Integer> list1 = new ArrayList<>();
+
+        ListNode current = head;
+        while (current != null) {
             list1.add(current.val);
-            current=current.next;
+            current = current.next;
         }
-        List<Integer> list2 =new ArrayList<>(list1);
+        List<Integer> list2 = new ArrayList<>(list1);
         Collections.reverse(list2);
         return list1.equals(list2);
     }
+
+public boolean isPalindrome(ListNode head) {
+    Stack<Integer> st=new Stack();
+    ListNode current = head;
+    while(current != null){
+    st.push(current.val);
+    current =current.next;
+    }
+
+    current =head;
+    while(current != null){
+        if( st.peek() != current.val){
+            return false;
+        } 
+        else {
+        current =current.next;
+        st.pop();
+        }
+    } 
+    return true;
+
+}
+
+
+
 }
