d3fc54db95f9307cd20a32a6a76e1569dc9dccf7
geetaPtongle <geetatongle@gmail.com>
2025-05-13 19:36:20 +0530

    Time: 0 ms (100%), Space: 42.2 MB (92.87%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.java
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        int count =0;
-        for(int i=0; i< arr.length;i++){
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 != 0) {
-            count++;
-            if (count == 3) {
-                return true;
+                count++;
+                if (count == 3) {
+                    return true;
+                }
+            } else {
+                count = 0;
             }
-        } else {
-            count = 0; // reset count if the number is even
-        }           
         }
         return false;
     }
