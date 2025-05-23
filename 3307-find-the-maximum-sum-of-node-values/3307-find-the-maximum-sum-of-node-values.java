2014524f60e221911513083abc3ecdcd8b78b520
geetaPtongle <geetatongle@gmail.com>
2025-05-23 13:55:28 +0530

    Time: 37 ms (5.26%), Space: 52.7 MB (97.37%) - LeetHub

--- 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.java
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.java
@@ -1,5 +1,5 @@
 class Solution {
-    public long maximumValueSum(int[] nums, int k, int[][] edges) {
+    public long maximumValueSum1(int[] nums, int k, int[][] edges) {
         long sum=0;
     int count =0;
     int loss=Integer.MAX_VALUE;
@@ -16,4 +16,28 @@ class Solution {
     if(count % 2==0) return sum;
     return sum-loss;
     }
+
+ public long maximumValueSum(int[] nums, int k, int[][] edges) {
+            int n = nums.length;
+            Long[] profit = new Long[n];
+            long orgSum = 0;
+
+            for (int i = 0; i < n; i++) {
+                profit[i] = (long)(nums[i] ^ k) - nums[i];
+                orgSum += nums[i];
+            }
+
+            // Sort profits in descending order
+            Arrays.sort(profit, Collections.reverseOrder());
+
+            for (int i = 0; i + 1 < n; i += 2) {
+                long pairSum = profit[i] + profit[i + 1];
+                if (pairSum > 0) {
+                    orgSum += pairSum;
+                } else {
+                    break; // No point in adding negative or zero profit pairs
+                }
+            }
+            return orgSum;
+        }
 }
