dee938200abf599ef42d048512eb0012126e9ce5
geetaPtongle <geetatongle@gmail.com>
2025-05-24 00:02:10 +0530

    Time: 4 ms (92.25%), Space: 45.9 MB (16.24%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
@@ -2,27 +2,22 @@ class Solution {
     public boolean checkValid(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        for (int i = 0; i < n; i++) {
+        for (int row = 0; row < n; row++) {
             boolean[] seenRow = new boolean[n];
-            for (int j = 0; j < m; j++) {
-                int numberIndex = matrix[i][j] - 1;
+            for (int col = 0; col < m; col++) {
+                int numberIndex = matrix[row][col] - 1;
                 if (seenRow[numberIndex])
                     return false;
                 seenRow[numberIndex] = true;
             }
         }
          for (int col = 0; col < n; ++col) {
-            // Create a tracker for seen numbers in the current column
             boolean[] seenColumn = new boolean[n];
             for (int row = 0; row < n; ++row) {
-                // Retrieve the number and convert it to an index (0 to n-1)
                 int numberIndex = matrix[row][col] - 1;
-                // Check if the number has already been seen in the current column
                 if (seenColumn[numberIndex]) {
-                    // If the number is seen before, the matrix is not valid
                     return false;
                 }
-                // Mark the number as seen
                 seenColumn[numberIndex] = true;
             }
         }
