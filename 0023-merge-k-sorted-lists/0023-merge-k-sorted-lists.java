697fce864975d92538d9072c62adee7eb44ec77e
geetaPtongle <geetatongle@gmail.com>
2025-05-26 22:27:50 +0530

    Time: 4 ms (73.65%), Space: 44.5 MB (64.08%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,6 +9,7 @@
  * }
  */
 class Solution {
+    //Using Priorityqueue Approch
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b) -> Integer.compare(a.val, b.val));
         for(ListNode node : lists){
@@ -33,5 +34,27 @@ class Solution {
             }
         }
         return head;
+    }
+
+//Using Divide and conquer approch
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (l1 != null) curr.next = l1;
+        else curr.next = l2;
+
+        return dummy.next;
     }
 }
