fef19095af1080cc246100e7b3f9e40e6789531f
geetaPtongle <geetatongle@gmail.com>
2025-06-06 21:39:23 +0530

    Time: 5 ms (63.41%), Space: 44.8 MB (51.68%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,40 +1,41 @@
 class Solution {
     public int[] minOperations1(String boxes) {
-        int n=boxes.length();
-        int[] ans=new int[n];     
-        int total=0;
-            for(int i=0; i<n; i++){
-                total=0;
-                for(int j=0; j<n ;j++){
-                    if(i==j) continue;
-                    int abs=Math.abs(j-i); 
-                     int val=Character.getNumericValue(boxes.charAt(j));
-                    total += abs*val;
-                }
-                ans[i] =total;
+        int n = boxes.length();
+        int[] ans = new int[n];
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+                int abs = Math.abs(j - i);
+                int val = Character.getNumericValue(boxes.charAt(j));
+                total += abs * val;
             }
-            return ans;
+            ans[i] = total;
+        }
+        return ans;
     }
 
     public int[] minOperations(String boxes) {
-         int n=boxes.length();
-        int[] ans=new int[n];     
-        int cumVal=0;
-        int cumSum=0;
-        for(int i=0; i< n; i++){
-            ans[i] +=cumSum;
+        int n = boxes.length();
+        int[] ans = new int[n];
+        int cumVal = 0;
+        int cumSum = 0;
+        for (int i = 0; i < n; i++) {
+            ans[i] += cumSum;
             int val = Character.getNumericValue(boxes.charAt(i));
             cumVal += val;
-        cumSum += cumVal;
+            cumSum += cumVal;
         }
-        cumVal=0;
-        cumSum=0;
+        cumVal = 0;
+        cumSum = 0;
 
-         for(int i=n-1;  i>=0; i--){
-            ans[i] +=cumSum;
-             int val = Character.getNumericValue(boxes.charAt(i));
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] += cumSum;
+            int val = Character.getNumericValue(boxes.charAt(i));
             cumVal += val;
-            cumSum +=cumVal;
+            cumSum += cumVal;
         }
         return ans;
     }
