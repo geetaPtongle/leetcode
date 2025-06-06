049f7bb5fdc313093327b0ab866bf1d9aefbcce9
geetaPtongle <geetatongle@gmail.com>
2025-06-06 21:39:04 +0530

    Time: 5 ms (63.41%), Space: 44.9 MB (51.68%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,11 +1,7 @@
 class Solution {
-    public int[] minOperations(String boxes) {
+    public int[] minOperations1(String boxes) {
         int n=boxes.length();
-        int[] ans=new int[n];
-        // Set<Integer> indices = IntStream.range(0, n)
-        //     .filter(i -> binary.charAt(i) == '1')
-        //     .boxed()
-        //     .collect(Collectors.toSet());
+        int[] ans=new int[n];     
         int total=0;
             for(int i=0; i<n; i++){
                 total=0;
@@ -18,5 +14,28 @@ class Solution {
                 ans[i] =total;
             }
             return ans;
+    }
+
+    public int[] minOperations(String boxes) {
+         int n=boxes.length();
+        int[] ans=new int[n];     
+        int cumVal=0;
+        int cumSum=0;
+        for(int i=0; i< n; i++){
+            ans[i] +=cumSum;
+            int val = Character.getNumericValue(boxes.charAt(i));
+            cumVal += val;
+        cumSum += cumVal;
+        }
+        cumVal=0;
+        cumSum=0;
+
+         for(int i=n-1;  i>=0; i--){
+            ans[i] +=cumSum;
+             int val = Character.getNumericValue(boxes.charAt(i));
+            cumVal += val;
+            cumSum +=cumVal;
+        }
+        return ans;
     }
 }
