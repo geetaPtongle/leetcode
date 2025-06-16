86fe875dfbd6a9d40756783759cdfae55b727d9b
geetaPtongle <geetatongle@gmail.com>
2025-06-16 21:11:41 +0530

    Time: 0 ms (100%), Space: 41 MB (65.94%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -1,29 +1,46 @@
 class Solution {
-    List<List<Integer>> subsets=new ArrayList<>();
+    //Approach-1 (Using simple backtracking to generate subsets)
+    //T.C : O(n * 2^n)
+    //S.C : O(n * 2^n)
+    // List<List<Integer>> subsets = new ArrayList<>();
+
+    // public int subsetXORSum1(int[] nums) {
+    //     List<Integer> temp = new ArrayList<>();
+    //     solve(0, nums, temp);
+
+    //     int result = 0;
+    //     for (List<Integer> subset : subsets) {
+    //         int xor = 0;
+    //         for (int num : subset) {
+    //             xor ^= num;
+    //         }
+    //         result += xor;
+    //     }
+    //     return result;
+    // }
+
+    // void solve(int index, int[] nums, List<Integer> temp) {
+    //     if (index >= nums.length) {
+    //         subsets.add(new ArrayList(temp));
+    //         return;
+    //     }
+
+    //     temp.add(nums[index]);
+    //     solve(index + 1, nums, temp);
+    //     temp.remove(temp.size() - 1);
+    //     solve(index + 1, nums, temp);
+    // }
+
     public int subsetXORSum(int[] nums) {
-        List<Integer> temp=new ArrayList<>();
-        solve(0, nums, temp);
-
-        int result =0;
-        for (List<Integer> subset : subsets) {
-            int xor=0;
-            for(int num: subset){
-                xor ^=num;
-            }
-            result +=xor;
-        }
-        return result;
+        return helper(0, nums, 0);
     }
 
-    void solve(int index, int[] nums, List<Integer> temp){
-        if(index >= nums.length){
-            subsets.add(new ArrayList(temp));
-            return;
-        }
+    int helper(int index, int[] nums, int xor){
+        if(index == nums.length) return xor;
+        int take =helper(index +1, nums, nums[index]^xor);
+        int exclude=helper(index+1, nums, xor);
 
-        temp.add(nums[index]);
-        solve(index +1, nums, temp);
-        temp.remove(temp.size() -1);
-         solve(index +1, nums, temp);
+        return take + exclude;
     }
+
 }
