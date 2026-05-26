bdf131876442e760634db2805b07dbf9b0fafc58
geetaPtongle <geetatongle@gmail.com>
2026-05-26 09:52:01 +0530

    Time: 1 ms (99.86%), Space: 55.4 MB (68.99%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,57 +1,43 @@
 class Solution {
-    // Brute forse approch
-    
-public int majorityElement1(int[] nums) {
-        int n = nums.length, count = 0;
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-
+    public int majorityElement1(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i < n;i++){
+             int count =0;
+            int temp=nums[i];
+            for(int j=0; j<n;j++){
+                if(temp == nums[j])
+                count++;
             }
-            if (count > n / 2)
-                result = nums[i];
-        }
-        return result;
-    }
-
-//using Hashing 
-    public int majorityElement2(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
-        }
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > nums.length / 2) {
-                return entry.getKey();
-            }
+        if(count > n/2)
+        return temp;
         }
         return -1;
+        
     }
-//
- public int majorityElement(int[] nums) {
+public int majorityElement(int[] nums) {
     int n=nums.length;
-    int count =0;
-    int el =0;
-    for(int i=0; i<n; i++){
-        if(count ==0){
-            count=1;
-            el=nums[i];
-        }        
-        else if(nums[i] == el) count++;
-        else count--;
-    }
+    int el=0;
     int count1=0;
 
-    for(int i=0; i< n; i++){
-        if(nums[i]== el) count1++;
+    for(int i=0; i<n; i++){
+        if(count1==0){
+            el= nums[i];
+            count1=1;
+        }
+        else if(nums[i] ==el){
+            count1++;
+        } 
+        else {count1--;}        
     }
-    if(count1 > n/2) return el;
-    else return -1;
- }
 
+    int count2=0;
+    for(int i=0;i<n; i++){
+        if(nums[i] ==el) count2++;
+    }
 
+    if(count2 > (n/2)) return el;
+    
+    return -1;
+    }
+    
 }
