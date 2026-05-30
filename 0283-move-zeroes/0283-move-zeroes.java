3f1f103dd3f96e2cca7420428180f274e3987ddf
geetaPtongle <geetatongle@gmail.com>
2026-05-30 15:31:21 +0530

    Time: 2 ms (91.69%), Space: 48 MB (18.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
+    public void moveZeroes1(int[] nums) {
         int n= nums.length;
         int[] temp = new int[nums.length];
         int index =0;
@@ -11,6 +11,27 @@ class Solution {
         }
         for(int i=0; i<n; i++){
             nums[i] =temp[i];
+        }
+    }
+    public void moveZeroes(int[] nums) {
+        int n= nums.length;
+        int j=-1;
+        //Finding first 0 element
+        for(int i=0; i<n; i++){
+            if(nums[i] ==0){
+                j=i;
+                 break;
+            }
+        }
+        if (j == -1) return;
+
+        for(int i= j+1; i<n; i++){
+            if(nums[i] !=0){
+                int temp =nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
         }
     }
 }
