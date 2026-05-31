73618eb777ec7109856a173e3150fb0cfc75fdde
geetaPtongle <geetatongle@gmail.com>
2026-05-31 16:50:58 +0530

    Time: 8 ms (42.03%), Space: 46.9 MB (82.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,7 +1,33 @@
 class Solution {
-    
+    public int[] twoSum(int[] nums, int target) {
+      
+          int n = nums.length;
 
-  public int[] twoSum(int[] nums, int target) {
+        // Step 1: store [value, original_index]
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums[i]; // value
+            arr[i][1] = i;       // original index
+        }
+
+        // Step 2: sort by value
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+          int i = 0, j = nums.length - 1;
+        while (i < j) {
+           int sum = arr[i][0] + arr[j][0];
+            if (sum == target) {
+                return new int[]{arr[i][1], arr[j][1]}; // 1-based indexing
+            } else if (sum < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return null;
+    }
+
+  public int[] twoSum1(int[] nums, int target) {
      HashMap<Integer, Integer> map = new HashMap<>(); // num -> index
 
         for (int i = 0; i < nums.length; i++) {
