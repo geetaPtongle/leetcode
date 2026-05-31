25ae03bbbf1c278b4220287481524f731c6171e2
geetaPtongle <geetatongle@gmail.com>
2026-05-31 14:50:59 +0530

    Time: 1 ms (99.97%), Space: 47.1 MB (27.42%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -6,4 +6,27 @@ class Solution {
         }
         return xor;
     }
+     public int singleNumber1(int[] nums) {
+        int n=nums.length;
+        int max= nums[0];
+        //finding max element
+        for(int i=0; i<n; i++){
+            max= Math.max(max, nums[i]);
+        }
+
+        //create hash array with max + 1 size
+        int[] hash =new int[max+1];
+
+        //count frecuency 
+        for(int i=0; i<n; i++){
+            hash[nums[i]]++;
+        }
+
+        //find out the element which has 1 fecency
+        for(int i=0; i<hash.length; i++){
+            if(hash[nums[i]] == 1) return nums[i];
+        }
+        return -1;
+     }
+
 }
