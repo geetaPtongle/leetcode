528c563a0b2bd4909b6f29e3529585b7d3197cc3
geetaPtongle <geetatongle@gmail.com>
2026-06-05 13:38:10 +0530

    Time: 0 ms (100%), Space: 44.8 MB (30.58%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,43 +1,40 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int piv = -1;
-        int n = nums.length;
+        int piv=-1;
+        int n=nums.length;
 
-        // FIX 1: start from n-2 to avoid i+1 out of bounds
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                piv = i;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i]< nums[i+1]){
+                piv=i;
+                break;
+            }
+        }
+            if(piv ==-1){
+                reverse(nums, 0, n-1);
+                return;
+            
+        }
+
+        for(int i=n-1; i>piv; i--){
+            if(nums[i] > nums[piv]){
+                int temp =nums[piv];
+                nums[piv]=nums[i];
+                nums[i]=temp;
                 break;
             }
         }
 
-        // FIX 2: reverse whole array if no pivot
-        if (piv == -1) {
-            reverse(nums, 0, n - 1);
-            return;
-        }
-
-        // find next greater element from right
-        for (int i = n - 1; i > piv; i--) {
-            if (nums[i] > nums[piv]) {
-                int temp = nums[piv];
-                nums[piv] = nums[i];
-                nums[i] = temp;
-                break;
-            }
-        }
-
-        // reverse suffix
-        reverse(nums, piv + 1, n - 1);
+          reverse(nums, piv + 1, n - 1);
     }
 
-    public void reverse(int[] nums, int i, int j) {
-        while (i < j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
-        }
-    }
+     public void reverse(int[] nums, int i, int j) {
+
+     while(i<=j){
+          int temp =nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+                j--;
+     }
+     }
 }
