1ffc035bd7a08c869ba3e222a1d57294542851cd
geetaPtongle <geetatongle@gmail.com>
2026-06-06 11:48:30 +0530

    Time: 3 ms (73.83%), Space: 52.7 MB (27.62%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<Integer> majorityElement(int[] nums) {
+    public List<Integer> majorityElement1(int[] nums) {
        int n = nums.length;
         List<Integer> result = new ArrayList<>();
 
@@ -22,6 +22,40 @@ class Solution {
             }
         }
 
+        return result;
+    }
+
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result=new ArrayList<>();
+        int count1=0, count2=0;
+        int el1=0,el2=0;
+        int n=nums.length;
+        for(int i=0; i<n; i++){
+
+            if(count1 ==0 && nums[i] !=el2){
+                count1=1;
+                el1=nums[i];
+            }else if(count2==0 && nums[i] !=el1){
+                count2=1;
+                el2=nums[i];
+            }else if(nums[i] ==el1) {
+                count1++;
+            } else if (nums[i] ==el2) {
+                count2++;
+            }else {
+                count1--;
+                count2--;
+            }
+        }
+        count1=0;
+        count2=0;
+        int mini = n / 3 + 1;
+        for (int i=0; i<n ;i++){
+            if(nums[i] ==el1) count1++;
+            if (nums[i] ==el2) count2++;
+        }
+        if(count1 >= mini) result.add(el1);
+        if (count2 >=mini && el1 !=el2) result.add(el2);
         return result;
     }
 }
